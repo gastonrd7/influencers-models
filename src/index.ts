7e3af17e12535254733c67040a3bce54f6ce74b1
Mario Ruiz Diaz <mariogrd@gmail.com>
2022-11-29 12:27:21 -0300

    new enums for syncing the api with client Apps

--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,14 @@ enum Model {
     transaction = "transaction"
 }
 
+enum General {
+    backendInstanceId = "backendinstanceid",
+}
+
+enum Messages {
+    RESET_NEEDED = "RESET_NEEDED"
+}
+
 enum transactionItemFields {
     _id = "_id",
     createdAt = "createdAt"
@@ -1195,5 +1203,7 @@ export {
     campaignPaymentStatusEnum,
     transactionItemTransactionFields,
     transactionFields,
-    knownErrorsEnum
+    knownErrorsEnum,
+    General,
+    Messages
 };
